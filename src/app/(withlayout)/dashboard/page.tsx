d3fb4514ab09d25bc1e4d401bd8649b882858e74
rahulhasan31 <rahulhasan666@gmail.com>
2024-07-17 22:10:23 +0600

    add

--- src/app/(withlayout)/dashboard/page.tsx
+++ src/app/(withlayout)/dashboard/page.tsx
@@ -43,47 +43,58 @@ const MyProfile = () => {
     <>
       {(isLoggedIn && userRole === "user") || userRole === "admin" ? (
         <>
-          <div className="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-            <div className="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-              <div className=" h-32 overflow-hidden">
-                <img
-                  className="w-full"
-                  src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="flex justify-center px-5  -mt-12">
-                <img
-                  className="h-32 w-32 bg-white p-2 rounded-full   "
-                  src="https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
-                  alt=""
-                />
-              </div>
-              <div className=" ">
-                <div className="text-center px-14">
-                  <h2 className="text-gray-800 text-3xl font-bold">
-                    {userData?.data.userName}
-                  </h2>
-                  <a className="text-gray-400 mt-2 hover:text-blue-500" href="">
-                    {userData?.data.userEmail}
-                  </a>
-                </div>
-                <hr className="mt-6" />
-                <div className="flex  bg-gray-50 ">
-                  <div className="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
-                    <p>
-                      <span className="font-semibold">2.5 k </span> Followers
-                    </p>
-                  </div>
-                  <div className="border"></div>
-                  <div className="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
-                    <p>
-                      {" "}
-                      <span className="font-semibold">2.0 k </span> Following
-                    </p>
-                  </div>
-                </div>
-              </div>
+          <div className="max-w-2xl mx-4 sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto mt-16 bg-white shadow-xl rounded-lg text-gray-900">
+            <div className="rounded-t-lg h-32 overflow-hidden">
+              <img
+                className="object-cover object-top w-full"
+                src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+                alt="Mountain"
+              />
+            </div>
+            <div className="mx-auto w-32 h-32 relative -mt-16 border-4 border-white rounded-full overflow-hidden">
+              <img
+                className="object-cover object-center h-32"
+                src="https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg"
+                alt="Woman looking front"
+              />
+            </div>
+            <div className="text-center mt-2">
+              <h2 className="font-semibold">{userData?.data?.userName}</h2>
+              <p className="text-gray-500">{userData?.data.userEmail}</p>
+            </div>
+            <ul className="py-4 mt-2 text-gray-700 flex items-center justify-around">
+              <li className="flex flex-col items-center justify-around">
+                <svg
+                  className="w-4 fill-current text-blue-900"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20">
+                  <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+                </svg>
+                <div>2k</div>
+              </li>
+              <li className="flex flex-col items-center justify-between">
+                <svg
+                  className="w-4 fill-current text-blue-900"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20">
+                  <path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z" />
+                </svg>
+                <div>10k</div>
+              </li>
+              <li className="flex flex-col items-center justify-around">
+                <svg
+                  className="w-4 fill-current text-blue-900"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20">
+                  <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
+                </svg>
+                <div>15</div>
+              </li>
+            </ul>
+            <div className="p-4 border-t mx-8  ">
+              <button className="w-1/2 block mx-auto rounded-full bg-gray-900 hover:shadow-lg font-semibold text-white px-6 py-2">
+                Update
+              </button>
             </div>
           </div>
         </>
